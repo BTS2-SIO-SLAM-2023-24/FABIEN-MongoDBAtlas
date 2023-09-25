7aa1d296ffb394df41facefccb01079aa3e50200
Fabien Michel <f.michel@swsio.local>
2023-09-25 15:50:12 +0200

    initial commit

---
+++ db.ts
@@ -0,0 +1,17 @@
+// db.ts
+import mongoose from 'mongoose';
+// Remplacez les valeurs entre < > par vos informations
+const connectionString = 'mongodb+srv://fabienmclll:jFKBiVfH8AGBN1ui@fabien.wvn8mqi.mongodb.net/?retryWrites=true&w=majority;
+
+export const connectToDatabase = async (): Promise<void> => {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connecté à MongoDB Atlas');
+  } catch (error) {
+    console.error('Erreur de connexion à MongoDB :', error);
+    process.exit(1); // Quitte l'application en cas d'erreur de connexion
+  }
+};
